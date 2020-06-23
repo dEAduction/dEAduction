42bc4ed6ec298a09f06ab3dbec6442a589b686b0
Kryzar <antoine@hugounet.com>
2020-06-23 11:32:50 +0200

    snippets(PySide2): finish a tutorial

--- snippets/PySide2/QtforPythonTutorials/YourFirstQtWidgetsApplication.py
+++ snippets/PySide2/QtforPythonTutorials/YourFirstQtWidgetsApplication.py
@@ -2,3 +2,11 @@
 Your First QtWidgets Application
 https://doc.qt.io/qtforpython/tutorials/basictutorial/widgets.html
 """
+
+import sys
+from PySide2.QtWidgets import QApplication, QLabel
+
+app = QApplication([])
+label = QLabel("<font color=red size=40>Hello World!</font>")
+label.show()
+app.exec_()
