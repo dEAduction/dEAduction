bf1ceb0ee88ceb4ef65f6be53e16dc533600b6d9
Kryzar <antoine@hugounet.com>
2020-06-27 00:07:40 +0200

    snippets(PySide2):finish ZetCode tutorial 'PyQt5 message box'

--- snippets/ZetCode PyQt5 tutorial/First programs in PyQt5/messagebox.py
+++ snippets/ZetCode PyQt5 tutorial/First programs in PyQt5/messagebox.py
@@ -21,11 +21,34 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle("Message box")
         self.show()
 
-    def closeEvent(self, event):
+    # closeEvent est une méthode de la classe parente QWidget
+    # et est appelée lorsque l'on essaie de fermer le QWidget courrant.
+    # C'est pour cela qu'il suffit de la déclarer et qu'il n'y a pas 
+    # besoin de l'appeler dans le programme. On remarquera par ailleurs
+    # que cela ne marche plus si l'on change le nom de closeEvent.
+    # https://doc.qt.io/qt-5/qcloseevent.html#QCloseEvent
 
-        pass
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, "Message",
+                                    "Are you sure to quit?", 
+                                    QMessageBox.Yes | QMessageBox.No,
+                                    QMessageBox.No) # la réponse par défaut
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
