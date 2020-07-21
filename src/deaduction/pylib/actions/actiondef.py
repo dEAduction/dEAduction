6978eefa5f6373421a9e6f2409eb0808d4dba82d
m-bin <mbin@student.42.fr>
2020-07-21 15:36:36 +0200

    docs : include headers

--- src/deaduction/pylib/actions/actiondef.py
+++ src/deaduction/pylib/actions/actiondef.py
@@ -1,13 +1,38 @@
+"""
+############################################################
+# actiondef.py : defines class Action                      #
+############################################################
+
+
+Author(s)     : - Marguerite Bin <bin.marguerite@gmail.com>
+Maintainer(s) : - Marguerite Bin <bin.marguerite@gmail.com>
+Created       : July 2020 (creation)
+Repo          : https://github.com/dEAduction/dEAduction
+
+Copyright (c) 2020 the dEAduction team
+
+This file is part of dEAduction.
+
+    dEAduction is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    dEAduction is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with dEAduction.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 
 import deaduction.pylib.logger as logger
 import logging
 import inspect
 from dataclasses import dataclass
 
-
-############################################
-# Very high meta stuff
-############################################
 @dataclass
 class Action:
     """
--- src/deaduction/pylib/actions/logic.py
+++ src/deaduction/pylib/actions/logic.py
@@ -1,6 +1,39 @@
+"""
+############################################################
+# logic.py : functions to call in order to                 #
+# translate actions into lean code                         #
+############################################################
+    
+
+Author(s)     : - Marguerite Bin <bin.marguerite@gmail.com>
+Maintainer(s) : - Marguerite Bin <bin.marguerite@gmail.com>
+Created       : July 2020 (creation)
+Repo          : https://github.com/dEAduction/dEAduction
+
+Copyright (c) 2020 the dEAduction team
+
+This file is part of dEAduction.
+
+    dEAduction is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    dEAduction is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with dEAduction.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from deaduction.pylib.actions.actiondef import action
 
 from dataclasses import dataclass
+
+import gettext
+_ = gettext.gettext
 
 import deaduction.pylib.logger as logger
 import logging
@@ -10,39 +43,32 @@ import logging
 # Squelette actions logiques
 ##
 
-@action("Négation")
+@action(_("Negation"))
 def action_negate(goal,):
-    log.info("négation")
     return ""
 
-@action("Implication")
+@action(_("Implication"))
 def action_implicate(a):
-    log.info("implication")
     return ""
 
-@action("Et")
+@action(_("And"))
 def action_and(a):
-    log.info("et")
     return ""
 
-@action("Ou")
+@action(_("Or"))
 def action_or(a):
-    log.info("ou")
     return ""
 
-@action("Si et seulement si")
+@action(_("If and only if"))
 def action_iff(a):
-    log.info("si et seulement si")
     return ""
 
-@action("Pour tout")
+@action(_("For all"))
 def action_forall(a):
-    log.info("pour tout")
     return ""
 
-@action("Il existe")
+@action(_("Exists"))
 def action_exists(a):
-    log.info("il existe")
     return ""
 
 if __name__ == "__main__":
