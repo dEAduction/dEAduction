5259663337e58ddd89b24544679a50d47be70097
Kryzar <antoine@hugounet.com>
2020-07-25 12:22:40 +0200

    feat(dui/widgets): create _TagIcon class

--- src/deaduction/dui/widgets/context_widgets_classes.py
+++ src/deaduction/dui/widgets/context_widgets_classes.py
@@ -26,20 +26,31 @@ This file is part of d∃∀duction.
 """
 
 from pathlib import Path
+from PySide2.QtGui import QPixMap
 from PySide2.QtWidgets import QHBoxLayout, QPushButton, QLabel
+
+
+class _TagIcon(QLabel):
+
+    def __init__(self, tag: str):
+        super().__init__()
+        icons_folder = Path('../graphical_resources/')
+            
+        if tag not in ['=', '+', '≠']:
+            raise ValueError('tag must be one of "=", "+", "≠". tag: {tag}.')
+        elif tag == '=':
+            return None
+        elif tag == '+':
+            icon_path = icons_folder / 'icon_tag_plus.png'
+        elif tag == '≠':
+            icon_path = icons_folder / 'icon_tag_different.png'
+
+        icon_pixmap = QPixmap(str(icon_path.resolve()))
+        icon_pixmap = icon_pixmap.scaledToWidth(10)
+        self.setPixmap(icon_pixmap)
+
 
 class ProofStatePOLayout(QHBoxLayout):
 
     def __init__(self, proofstatepo: ProofStatePO, tag: str):
         super().__init__()
-
-    @property
-    def tag(self):
-        return self._tag
-
-    @tag.setter
-    def tag(self, tag):
-        icons_folder = Path('../graphical_resources/')
-
-        if tag not in ['+', '≠', '=']:
-            raise ValueError('ProofStatePOLayout.tag must be one of "+", "="'
