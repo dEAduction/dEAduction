2eea91975f18d1af924ff3cf5ddb106d9fd1bd92
m-bin <mbin@student.42.fr>
2020-07-27 18:07:32 +0200

    tools: add __init__ in actions directory

---
+++ src/deaduction/pylib/actions/__init__.py
@@ -0,0 +1,33 @@
+"""
+# __init__.py : #ShortDescription #
+    
+    (#optionalLongDescription)
+
+Author(s)     : Marguerite Bin bin.marguerite@gmail.com
+Maintainer(s) : Marguerite Bin bin.marguerite@gmail.com
+Created       : 07 2020 (creation)
+Repo          : https://github.com/dEAduction/dEAduction
+
+Copyright (c) 2020 the dEAduction team
+
+This file is part of dEAduction.
+
+    dEAduction is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    dEAduction is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with dEAduction.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .actiondef import Action
+
+from .logic import action_negate, action_implicate, action_and, action_or, action_forall, action_exists
+
+from .proofs import action_assumption, action_absurdum, action_cbr, action_contrapose, action_contradiction
