254488ba8e4a5c1718cce636db3b001f5a80c6e6
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2020-07-31 17:30:01 +0200

    feat(config): basic config module

---
+++ src/deaduction/pylib/config/dirs.py
@@ -0,0 +1,57 @@
+"""
+###############################
+# dirs.py : Directories links #
+###############################
+
+Author(s)      : Florian Dupeyron <florian.dupeyron@mugcat.fr>
+Maintainers(s) : Florian Dupeyron <florian.dupeyron@mugcat.fr>
+Date           : July 2020
+
+Copyright (c) 2020 the dEAduction team
+
+This file is part of d∃∀duction.
+
+    d∃∀duction is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    d∃∀duction is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with d∃∀duction. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import logging
+from pathlib import Path
+from gettext import gettext as _
+
+log = logging.getLogger(__name__)
+
+############################################
+# Path objects
+############################################
+pkg_dir  = (Path(__file__) / "../../").resolve()
+
+# Share paths
+share    = (pkg_dir / "share").resolve()
+icons    = (share / "graphical_resources" / "icons").resolve()
+
+# Home paths
+home     = Path.home()
+local    = (home / ".deaduction").resolve()
+
+############################################
+# Utilities
+############################################
+def check_dir(pp):
+    log.info(_("Checking path: ") + str(pp))
+    if not pp.is_dir():
+        log.warning(_(f"Path {str(pp)} doesn't exists, creating"))
+        pp.mkdir()
+
+def init():
+    check_dir(local)
---
+++ src/deaduction/pylib/config/vars.py
@@ -0,0 +1,63 @@
+"""
+#########################################
+# vars.py : Get configuration variables #
+#########################################
+
+Author(s)      : Florian Dupeyron <florian.dupeyron@mugcat.fr>
+Maintainers(s) : Florian Dupeyron <florian.dupeyron@mugcat.fr>
+Date           : July 2020
+
+    The configuration variables are stored primarly in two files :
+        -> The user end configuration is in ~/.deaduction/config.toml
+        -> The factory configuration is in <deaduction package>/share/config.toml.
+
+    By default, user configuration has precedence over factory configuration.
+    the user should not have its own configuration, unless it is an advanced
+    user who which to mess with that stuff.
+
+Copyright (c) 2020 the dEAduction team
+
+This file is part of d∃∀duction.
+
+    d∃∀duction is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    d∃∀duction is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with d∃∀duction. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from gettext import gettext as _
+import logging
+from . import dirs
+
+FACTORY_CONFIG_FILE_PATH = (dirs.share / "config.toml").resolve()
+USER_CONFIG_FILE_PATH    = (dirs.local / "config.toml").resolve()
+
+dict_ = dict() # Contains loaded configuration
+
+log = logging.getLogger(__name__)
+
+def load():
+    log.info(_("Loading configuration file"))
+
+    # Load configuration file
+    dict_ = toml.load(str(CONFIG_FILE_PATH))
+
+def save():
+    log.info(_("Saving configuration file"))
+    toml.dump(dict_, CONFIG_FILE_PATH)
+
+def get(name: str):
+    return dict_[name]
+
+def set(name: str, value: any):
+    dict_[name] = value
+
+
