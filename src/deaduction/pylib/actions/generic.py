489946cbdb0889d3b596728a535e4c483da5a662
m-bin <mbin@student.42.fr>
2020-07-31 12:44:31 +0200

    fix: change prototype of functions action_theorem and action_apply_definition

--- src/deaduction/pylib/actions/generic.py
+++ src/deaduction/pylib/actions/generic.py
@@ -36,23 +36,23 @@ from deaduction.pylib.actions import (  WrongUserInput,
                                         utils)
 
 @action(_("Apply Definition"))
-def action_apply_definition(goal : Goal, selected_objects : [PropObj]):
-    if len(selected_objects) == 1:
-        defi = selected_objects[0].lean_data["name"]
+def action_apply_definition(goal : Goal, selected_objects : [PropObj], definition : Statement):
+    if len(selected_objects) == 0:
+        defi = definition.lean_name
         return "defi {0}".format(defi)
-    elif len(selected_objects) == 2:
+    elif len(selected_objects) == 1:
         defi = selected_objects[1].lean_data["name"]
         return "defi {0} at {1}".format(selected_objects[1].lean_data["name"], defi)
     else:
         raise WrongUserInput
 
 @action(_("Theorem"))
-def action_theorem(goal : Goal, selected_objects : [PropObj]):
-    theorem = selected_objects[0].lean_data["name"]
+def action_theorem(goal : Goal, selected_objects : [PropObj], theorem : Statement):
+    th = theorem.lean_name
     if len(selected_objects) == 1:
         h = utils.get_new_hyp()
-        return "apply {1} <|> have {0} := @{1},".format(h, theorem)
+        return "apply {1} <|> have {0} := @{1},".format(h, th)
     elif len(selected_objects) == 2:
         arguments = " ".join(selected_objects[1].lean_data["name"])
         h = utils.get_new_hyp()
-        return "apply {1} {2} <|> apply @{1} {2} <|> have {0} := {1} {2} <|> have {0} := @{1} {2},".format(h, theorem, argument)
+        return "apply {1} {2} <|> apply @{1} {2} <|> have {0} := {1} {2} <|> have {0} := @{1} {2},".format(h, th, argument)
