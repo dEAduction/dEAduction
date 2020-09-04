e375da4f2f2b4130ab77e38f3d99338413522946
Kryzar <antoine@hugounet.com>
2020-09-04 13:53:15 +0200

    snippets(launch_dialogs): refactor CourseChooseAndPreview

--- snippets/launch_dialogs/__main__.py
+++ snippets/launch_dialogs/__main__.py
@@ -2,6 +2,8 @@ import sys
 from pathlib import             Path
 from PySide2.QtCore import      Slot
 from PySide2.QtWidgets import ( QApplication,
+                                QGridLayout,
+                                QGroupBox,
                                 QFileDialog,
                                 QPushButton,
                                 QListWidget,
@@ -12,70 +14,85 @@ from PySide2.QtWidgets import ( QApplication,
                                 QHBoxLayout)
 
 
-@Slot()
-def browse_for_course():
-
-    dialog = QFileDialog()
-    dialog.setFileMode(QFileDialog.ExistingFile)
-    dialog.setNameFilter('*.lean')
-
-    if dialog.exec_():
-        course_file_path = Path(dialog.selectedFiles()[0])
-
-
-class CourseChooser(QWidget):
+class CourseChooseAndPreview(QGroupBox):
 
     def __init__(self):
         super().__init__()
+        self.setTitle('Choose course')
 
         # ─────────────── Widgets and layouts ────────────── #
-        
+
         browse_btn = QPushButton('Browse files')
-        browse_btn.clicked.connect(browse_for_course)
-        browse_btn_lyt = QHBoxLayout()
+        browse_btn.clicked.connect(self.__browse_for_course)
+        browse_btn_lyt = QVBoxLayout()
         browse_btn_lyt.addWidget(browse_btn)
         browse_btn_lyt.addStretch()
 
-        previous_courses_lyt = QVBoxLayout()
-        previous_courses_list_wgt = QListWidget()
-        previous_courses_list_wgt.itemClicked.connect(
+        previous_courses_wgt = QListWidget()
+        previous_courses_wgt.itemClicked.connect(
                 self.enable_choose_this_course_btn)
-        previous_courses_list_wgt.addItem(QListWidgetItem('Test item'))
-        previous_courses_lyt.addWidget(QLabel('Or choose a previous course'))
-        previous_courses_lyt.addWidget(previous_courses_list_wgt)
+        previous_courses_wgt.addItem(QListWidgetItem('Test item'))
 
-        buttons_lyt = QHBoxLayout()
-        help_btn = QPushButton('Get help')
-        quit_btn = QPushButton('Quit')
-        self.choose_this_course_btn = QPushButton('Choose this course')
-        self.choose_this_course_btn.setEnabled(False)
-        buttons_lyt.addWidget(help_btn)
-        buttons_lyt.addWidget(quit_btn)
-        buttons_lyt.addStretch()
-        buttons_lyt.addWidget(self.choose_this_course_btn)
+        choose_course_lyt = QHBoxLayout()
+        choose_course_lyt.addWidget(previous_courses_wgt)
+        choose_course_lyt.addLayout(browse_btn_lyt)
+
+        course_meta_data = QGridLayout()
+        course_meta_data.addWidget(QLabel('Adress:'),   0, 0)
+        course_meta_data.addWidget(QLabel('Teacher:'),  1, 0)
+        course_meta_data.addWidget(QLabel('Uni:'),      2, 0)
+        course_meta_data.addWidget(QLabel('Year:'),     3, 0)
+
+        # ─────────────────── Main layout ────────────────── #
 
         main_layout = QVBoxLayout()
-        main_layout.addLayout(browse_btn_lyt)
-        main_layout.addLayout(previous_courses_lyt)
+        main_layout.addWidget(QLabel('Choose a previous course or browse files'))
+        main_layout.addLayout(choose_course_lyt)
         main_layout.addStretch()
-        main_layout.addLayout(buttons_lyt)
+        main_layout.addWidget(QLabel('<i>Selected course</i>'))
+        main_layout.addLayout(course_meta_data)
         self.setLayout(main_layout)
 
-        # ─────────────────────── UI ─────────────────────── #
-
-        self.setWindowTitle('Choose a course file')
 
     @Slot()
     def enable_choose_this_course_btn(self):
 
         self.choose_this_course_btn.setEnabled(True)
-        
+
+    @Slot()
+    def __browse_for_course(self):
+
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.ExistingFile)
+        dialog.setNameFilter('*.lean')
+
+        if dialog.exec_():
+            course_file_path = Path(dialog.selectedFiles()[0])
+
+
+class ChooseCourseExercise(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        buttons_lyt = QHBoxLayout()
+        help_btn = QPushButton('Get help')
+        quit_btn = QPushButton('Quit')
+        self.choose_this_course_btn = QPushButton('Choose this course')
+        buttons_lyt.addWidget(help_btn)
+        buttons_lyt.addWidget(quit_btn)
+        buttons_lyt.addStretch()
+        buttons_lyt.addWidget(self.choose_this_course_btn)
+
+        # ─────────────────────── UI ─────────────────────── #
+
+        self.setWindowTitle('Choose course and exercise')
 
 
 if __name__ == '__main__':
     app = QApplication()
 
-    cc = CourseChooser()
+    cc = CourseChooseAndPreview()
     cc.show()
 
     sys.exit(app.exec_())
