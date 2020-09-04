f15e9cc721adf0ff132a9e2f5c3840a9718e4b7f
Kryzar <antoine@hugounet.com>
2020-09-04 18:15:49 +0200

    snippets(choose_course_exercise_widget): a state to which I will want to go back

--- snippets/choose_course_exercise_widget/__main__.py
+++ snippets/choose_course_exercise_widget/__main__.py
@@ -1,5 +1,7 @@
 import sys
-from pathlib import             Path
+from pathlib import Path
+from typing import  OrderedDict
+
 from PySide2.QtCore import      Slot
 from PySide2.QtWidgets import ( QApplication,
                                 QGridLayout,
@@ -14,6 +16,28 @@ from PySide2.QtWidgets import ( QApplication,
                                 QHBoxLayout)
 
 
+class CourseExercisePreview(QWidget):
+
+    def __init__(self, title: str, metadata: OrderedDict[str, str]):
+        super().__init__()
+
+        grid_layout = QGridLayout()
+
+        for label, val in metadata.items():
+            row = grid_layout.rowCount()
+            grid_layout.addWidget(QLabel(f'{label}:'), row, 0)
+            grid_layout.addWidget(QLabel(val),         row, 1)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel(f'<i>{title}</i>'))
+        main_layout.addLayout(grid_layout)
+
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        
+        self.setLayout(main_layout)
+
+
+
 class CourseChooseAndPreview(QGroupBox):
 
     def __init__(self):
@@ -25,40 +49,24 @@ class CourseChooseAndPreview(QGroupBox):
 
         browse_btn = QPushButton('Browse files')
         browse_btn.clicked.connect(self.__browse_for_course)
-        browse_btn_lyt = QVBoxLayout()
-        browse_btn_lyt.addWidget(browse_btn)
-        browse_btn_lyt.addStretch()
-
         previous_courses_wgt = QListWidget()
-        previous_courses_wgt.itemClicked.connect(
-                self.enable_choose_this_course_btn)
         previous_courses_wgt.addItem(QListWidgetItem('Test item'))
-
-        choose_course_lyt = QHBoxLayout()
+        choose_course_lyt = QVBoxLayout()
+        choose_course_lyt.addWidget(QLabel('Choose a previous course or browse files'))
+        choose_course_lyt.addWidget(browse_btn)
         choose_course_lyt.addWidget(previous_courses_wgt)
-        choose_course_lyt.addLayout(browse_btn_lyt)
 
-        course_meta_data = QGridLayout()
-        course_meta_data.addWidget(QLabel('Adress:'),   0, 0)
-        course_meta_data.addWidget(QLabel('Teacher:'),  1, 0)
-        course_meta_data.addWidget(QLabel('Uni:'),      2, 0)
-        course_meta_data.addWidget(QLabel('Year:'),     3, 0)
+        metadata = {'Adress': '', 'Teacher': '', 'School': '', 'Year': ''}
+        course_meta_data = CourseExercisePreview('Selected course', metadata)
 
         # ─────────────────── Main layout ────────────────── #
 
         main_layout = QVBoxLayout()
-        main_layout.addWidget(QLabel('Choose a previous course or browse files'))
         main_layout.addLayout(choose_course_lyt)
         main_layout.addStretch()
-        main_layout.addWidget(QLabel('<i>Selected course</i>'))
-        main_layout.addLayout(course_meta_data)
+        main_layout.addWidget(course_meta_data)
         self.setLayout(main_layout)
 
-
-    @Slot()
-    def enable_choose_this_course_btn(self):
-
-        self.choose_this_course_btn.setEnabled(True)
 
     @Slot()
     def __browse_for_course(self):
@@ -78,6 +86,20 @@ class ExerciseChooseAndPreview(QGroupBox):
         super().__init__()
         self.setTitle('Choose exercise')
 
+        exercises_list = QListWidget()
+        exercises_list.addItem(QListWidgetItem(
+            "L'image réciproque d'un ouvert est un ouvert"))
+
+        metadata = {'Name': '', 'Instructions': '', 'Comments': '', 'L∃∀N code': ''}
+        exercise_preview_wgt = CourseExercisePreview('Selected exercise', metadata)
+
+        main_lyt = QVBoxLayout()
+        main_lyt.addWidget(QLabel('Exercises list'))
+        main_lyt.addWidget(exercises_list)
+        main_lyt.addStretch()
+        main_lyt.addWidget(exercise_preview_wgt)
+
+        self.setLayout(main_lyt)
 
 class ChooseCourseExercise(QWidget):
 
@@ -88,6 +110,7 @@ class ChooseCourseExercise(QWidget):
         course_cap =   CourseChooseAndPreview()
         exercise_cap = ExerciseChooseAndPreview()
         selection_zone_lyt = QHBoxLayout()
+        selection_zone_lyt.setContentsMargins(0, 0, 0, 0)
         selection_zone_lyt.addWidget(course_cap)
         selection_zone_lyt.addWidget(exercise_cap)
 
