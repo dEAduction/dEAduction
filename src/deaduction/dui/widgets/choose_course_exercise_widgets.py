56f99b17b6a541e78827b765e0527abcbda68b71
Kryzar <antoine@hugounet.com>
2020-10-03 20:00:21 +0200

    refactor(dui/widgets/CEx chooser): make left_layout mandatory in AbstractCExChooser.__init__

--- src/deaduction/dui/widgets/choose_course_exercise_widgets.py
+++ src/deaduction/dui/widgets/choose_course_exercise_widgets.py
@@ -28,40 +28,12 @@ This file is part of d∃∀duction.
 
 class AbstractCExChooser(QGroupBox):
 
-    def __init__(self, gb_title: str, left_layout: QLayout=None,
+    def __init__(self, gb_title: str, left_layout: QLayout,
                  right_layout: QLayout=None):
 
         super().__init__(gb_title)
-
-        if not left_layout and not right_layout:
-            main_layout = QVBoxLayout()
-            main_layout.addStretch()
-            main_layout.addWidget(_('Nothing to choose from.'))
-            main_layout.addStretch()
-
-            self.left_layout = QLayout()
-            self.right_layout = QLayout()
-            self.setLayout(main_layout)
-        else:
-            if left_layout:
-                self.left_layout = left_layout
-            else:
-                left_layout = QVBoxLayout()
-                left_layout.addStretch()
-                left_layout.addWidget(_('Nothing to browse.'))
-                left_layout.addStretch()
-
-                self.left_layout = QLayout()
-
-            if right_layout:
-                self.right_layout = right_layout
-            else:
-                right_layout = QVBoxLayout()
-                right_layout.addStretch()
-                right_layout.addWidget(_('Nothing to preview.'))
-                right_layout.addStretch()
-
-                self.right_layout = QLayout()
+        self.set_left_layout()
+        self.set_right_layout()
 
         main_layout = QHBoxLayout()
         main_layout.addLayout(self.left_layout)
@@ -73,47 +45,49 @@ class AbstractCExChooser(QGroupBox):
         delete_replace_widget(self.left_layout, layout)
         self.left_layout = layout
 
-    def set_right_layout(self, cls_metadata: Dict[str, Any]=None,
+    def set_right_layout(self, metadata: Dict[str, Any]=None,
                          layout: QLayout=None):
 
         right_layout = QVBoxLayout()
 
-        if cls_metadata:
+        if not metadata and not layout:
+            right_layout.addStretch()
+            right_layout.addWidget(_('Nothing to preview.'))
+            right_layout.addStretch()
+        else:
+            if metadata:
 
-            cls_metadata_lyt = QVBoxLayout()
+                if metadata['title']
+                    title_wgt = QLabel(metadata["title"])
+                    title_wgt.setStyleSheet('font-size: 16pt;' \
+                                            'font-weight: bold;')
 
-            if cls_metadata_lyt_data['title']
-                title_wgt = QLabel(cls_metadata_lyt_data["title"])
-                title_wgt.setStyleSheet('font-size: 16pt;' \
-                                        'font-weight: bold;')
-                self.cls_metadata_lyt.addWidget(title_wgt)
+                    right_layout.addWidget(title_wgt)
 
-            if cls_metadata_lyt_data['subtitle']:
-                subtitle_wgt = QLabel(subtitle)
-                subtitle_wgt.setStyleSheet('font-style: italic;' \
-                                           'color: gray;')
-                subtitle_lyt = QHBoxLayout()
-                subtitle_lyt.addWidget(title_wgt)
-                subtitle_lyt.addWidget(subtitle_wgt)
+                if metadata['subtitle']:
+                    subtitle_wgt = QLabel(subtitle)
+                    subtitle_wgt.setStyleSheet('font-style: italic;' \
+                                               'color: gray;')
+                    subtitle_lyt = QHBoxLayout()
+                    subtitle_lyt.addWidget(title_wgt)
+                    subtitle_lyt.addWidget(subtitle_wgt)
 
-                cls_metadata_lyt.addLayout(sub_title_lyt)
+                    right_layout.addLayout(sub_title_lyt)
 
-            if cls_metadata_lyt_data['details']:
-                details_wgt = DisclosureTree('Details',
-                                             cls_metadata_lyt_data['details'])
+                if metadata['details']:
+                    details_wgt = DisclosureTree('Details',
+                                                 metadata['details'])
 
-                cls_metadata_lyt.addWidget(details_wgt)
+                    right_layout.addWidget(details_wgt)
 
-            if cls_metadata_lyt_data['description']:
-                description_wgt = QLabel(cls_metadata_lyt_data['description'])
-                description_wgt.setWordWrap(true)
+                if metadata['description']:
+                    description_wgt = QLabel(metadata['description'])
+                    description_wgt.setWordWrap(true)
 
-                cls_metadata_lyt.addWidget(description_wgt)
+                    right_layout.addWidget(description_wgt)
 
-            right_layout.addLayout(cls_metadata_lyt)
-
-        if right_layout:
-            right_layout.addLayout(layout)
+            if layout:
+                right_layout.addLayout(layout)
 
         replace_delete_widget(self.right_layout, layout)
-        self.right_layout = layout
+        self.right_layout = right_layout
