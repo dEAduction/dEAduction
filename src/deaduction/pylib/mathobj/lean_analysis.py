6acb7949c7b816a06382885d12501c4d7ea05b9a
Frédéric Le Roux <frederic.le-roux@imj-prg.fr>
2020-11-17 12:14:17 +0100

    fix: adapt parser course for numbers (add field_name = value)

--- src/deaduction/pylib/mathobj/lean_analysis.py
+++ src/deaduction/pylib/mathobj/lean_analysis.py
@@ -61,7 +61,7 @@ details = """
             info_field (sep_slash info_field)*
             closed_bra
     info_field = info_field_name ": " info_field_content    
-    info_field_name = !"type" ("name" / "identifier" / "pp_type") 
+    info_field_name = !"type" ("name" / "identifier" / "pp_type" / "value") 
     info_field_content = any_non_sep_symbol+ 
     
     node_name = ~"[a-z A-Z 0-9 _∀∃+-<>≥≤!]"+
