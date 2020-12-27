215cd67cf1eea984dba75962da5942fbddaeadc6
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2020-12-27 20:31:39 +0100

    refactor(pylib.config.i18n): save available_languages as list instead of string

--- src/deaduction/pylib/config/i18n.py
+++ src/deaduction/pylib/config/i18n.py
@@ -46,7 +46,7 @@ def init():
     log.info("Init i18n")
 
     available_languages = cvars.get("i18n.available_languages", "en")
-    available_languages = available_languages.split(", ")
+    #available_languages = available_languages.split(", ")
 
     select_language     = cvars.get('i18n.select_language', "en")
 
