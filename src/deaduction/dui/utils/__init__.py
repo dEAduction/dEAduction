6a9449abfdedc66af014772793ff173757f6e384
Kryzar <antoine@hugounet.com>
2021-03-02 17:35:26 +0100

    chores(dui.utils): make __init__.py

---
+++ src/deaduction/dui/utils/__init__.py
@@ -0,0 +1,31 @@
+"""
+######################################################
+# __init__.py : __init__.py for deaduction.dui.utils #
+######################################################
+
+Author(s)      : Kryzar <antoine@hugounet.com>
+Maintainers(s) : Kryzar <antoine@hugounet.com>
+Date           : March 2021
+
+Copyright (c) 2021 the dEAduction team
+
+This file is part of d∃∀duction.
+
+    d∃∀duction is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    d∃∀duction is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with d∃∀duction. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .horizontal_line       import HorizontalLine
+from .read_pkl_course       import read_pkl_course
+from .replace_widget_layout import replace_widget_layout
+from .set_item_selectable   import set_item_selectable
