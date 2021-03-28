45a3537611d20c97d5cd017856a119de763e666b
Frédéric Le Roux <frederic.le-roux@imj-prg.fr>
2021-03-28 23:15:49 +0200

    feat(magic.py): change behaviour of CQFD button when some property is selected

--- src/deaduction/pylib/actions/magic.py
+++ src/deaduction/pylib/actions/magic.py
@@ -88,70 +88,75 @@ def action_assumption(goal: Goal, l: [MathObject]) -> str:
     :param l: list of MathObject arguments preselected by the user
     :return: string of lean code
     """
+
     # NB: this button should either solve the goal or fail.
     # For this we wrap the non "solve-or-fail" tactics
     # into the combinator "solve1"
 
     possible_codes = []
-    if len(l) == 0:
-        possible_codes.append('assumption')
-        possible_codes.append('contradiction')
-        if goal.target.is_equality() or goal.target.is_iff():
-            if goal.target.math_type.children[0] == \
-                    goal.target.math_type.children[1]:
-                possible_codes.append('refl')
-            # try to use associativity and commutativity
-            possible_codes.append('ac_reflexivity')
-            # try to permute members of the goal equality
-            possible_codes.append('apply eq.symm, assumption')
-            # congruence closure, solves e.g. (a=b, b=c : f a = f c)
-            possible_codes.append('cc')
-        # possible_codes.append('apply iff.symm, assumption')
-        elif goal.target.is_inequality():
-            # try to permute members of the goal equality
-            possible_codes.append('apply ne.symm, assumption')
+    # if len(l) == 0:
+    possible_codes.append('assumption')
+    possible_codes.append('contradiction')
 
-        # Try some symmetry rules
-        if goal.target.is_iff():
-            possible_codes.append('apply iff.symm, assumption')
-        elif goal.target.is_and():  # todo: change to "id_and_like"
-            possible_codes.append('apply and.symm, assumption')
-            possible_codes.append('split, assumption, assumption')
-        elif goal.target.is_or():
-            possible_codes.append('apply or.symm, assumption')
-            # The following is too much?
-            # possible_codes.append('left, assumption')
-            # possible_codes.append('right, assumption')
+    # (1) Equality, inequality, iff
+    if goal.target.is_equality() or goal.target.is_iff():
+        if goal.target.math_type.children[0] == \
+                goal.target.math_type.children[1]:
+            possible_codes.append('refl')
+        # try to use associativity and commutativity
+        possible_codes.append('ac_reflexivity')
+        # try to permute members of the goal equality
+        possible_codes.append('apply eq.symm, assumption')
+        # congruence closure, solves e.g. (a=b, b=c : f a = f c)
+        possible_codes.append('cc')
+    # possible_codes.append('apply iff.symm, assumption')
+    elif goal.target.is_inequality():
+        # try to permute members of the goal equality
+        possible_codes.append('apply ne.symm, assumption')
 
-        # Try to split hypotheses that are conjunctions
-        code = ""
-        counter = 0
-        for prop in goal.context:
-            if prop.is_and():
-                name = prop.info['name']
-                H0 = f"H_aux_{counter}"  # these hypotheses will disappear
-                H1 = f"H_aux_{counter + 1}"  # so names are unimportant
-                code += f"cases {name} with {H0} {H1}, "
-                counter += 2
-        if code:
-            code += 'assumption'
+    # (2) Try some symmetry rules
+    if goal.target.is_iff():
+        possible_codes.append('apply iff.symm, assumption')
+    elif goal.target.is_and():  # todo: change to "id_and_like"
+        possible_codes.append('apply and.symm, assumption')
+        possible_codes.append('split, assumption, assumption')
+    elif goal.target.is_or():
+        possible_codes.append('apply or.symm, assumption')
+        # The following is too much?
+        # possible_codes.append('left, assumption')
+        # possible_codes.append('right, assumption')
+
+    # (3) Conjunctions: try to split hypotheses
+    code = ""
+    counter = 0
+    for prop in goal.context:
+        if prop.is_and():
+            name = prop.info['name']
+            H0 = f"H_aux_{counter}"  # these hypotheses will disappear
+            H1 = f"H_aux_{counter + 1}"  # so names are unimportant
+            code += f"cases {name} with {H0} {H1}, "
+            counter += 2
+    if code:
+        code += 'assumption'
+        possible_codes.append(code)
+
+    # (4) Search for specific prop
+    for prop in goal.context:
+        math_type = prop.math_type
+        # Conclude from "x in empty set"
+        if (math_type.node == "PROP_BELONGS"
+                and math_type.children[1].node == "SET_EMPTY"):
+            H2 = prop.info['name']
+            if goal.target.node != "PROP_FALSE":
+                code = "exfalso, "
+            else:
+                code = ""
+            code += f"exact set.not_mem_empty _ {H2}"
             possible_codes.append(code)
-        # Search for specific prop
-        for prop in goal.context:
-            math_type = prop.math_type
-            # conclude from "x in emptyset"
-            if (math_type.node == "PROP_BELONGS"
-                    and math_type.children[1].node == "SET_EMPTY"):
-                H2 = prop.info['name']
-                if goal.target.node != "PROP_FALSE":
-                    code = "exfalso, "
-                else:
-                    code = ""
-                code += f"exact set.not_mem_empty _ {H2}"
-                possible_codes.append(code)
 
     if len(l) == 1:
         possible_codes.append(solve1_wrap(f'apply {l[0].info["name"]}'))
+
     if (goal.target.is_equality()
             or goal.target.is_inequality()
             or goal.target.is_false()):
