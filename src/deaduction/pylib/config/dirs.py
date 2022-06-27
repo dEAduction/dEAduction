d5c50a934ffa0a91fbcabb9d5f3321bd6d9467bc
Frédéric Le Roux <frederic.le-roux@imj-prg.fr>
2022-06-27 16:28:18 +0200

    code::fix> Put the leanpkg.path in $HOME

--- src/deaduction/pylib/config/dirs.py
+++ src/deaduction/pylib/config/dirs.py
@@ -55,6 +55,8 @@ test_exercises = (local / "test_exercises").resolve()
 all_courses_ipf_dir = (local / "initial_proof_states").resolve()
 all_courses_ipf_old = (local / "old_initial_proof_states").resolve()
 usr_lean_exercises_dir = (local / "lean_exercises_dir").resolve()
+lean_src_dir = (pkg_dir / "lean_src").resolve()
+usr_lean_rsc_dir = (local / "lean_src").resolve()
 lean = local / "lean"
 mathlib = local / "mathlib"
 
