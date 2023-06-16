8e58e6c73d94211e15f8b6e99e6851f87597dce9
Frederic Le Roux <frederic.le-roux@imj-prg.fr>
2023-06-16 22:58:34 +0200

    code::dev> Connect CalculatorButtons

--- src/deaduction/dui/stages/calculator/calculator.py
+++ src/deaduction/dui/stages/calculator/calculator.py
@@ -30,8 +30,10 @@ if __name__ == '__main__':
     language_check()
 
 import sys
-
+import logging
 from typing import Union, List
+
+from PySide2.QtCore import Signal, Slot
 
 from PySide2.QtWidgets import (QApplication, QWidget, QPushButton, QToolButton,
                                QHBoxLayout, QVBoxLayout, QLabel)
@@ -47,6 +49,7 @@ from deaduction.pylib.pattern_math_obj import (PatternMathObject,
 from deaduction.dui.elements import TargetLabel
 
 global _
+log = logging.getLogger(__name__)
 
 
 class CalculatorButton(QToolButton):
@@ -57,10 +60,18 @@ class CalculatorButton(QToolButton):
     construction.
     """
 
+    send_pattern = Signal(MarkedPatternMathObject)
+
     def __init__(self, symbol):
         super().__init__()
         self.pattern_s = CalculatorPatternLines.marked_patterns[symbol]
         self.setText(symbol)
+        self.clicked.connect(self.process_click)
+
+    @Slot()
+    def process_click(self):
+        # print('clic')
+        self.send_pattern.emit(self.pattern_s)
 
 
 class CalculatorButtons(QHBoxLayout):
@@ -83,6 +94,8 @@ class CalculatorMainWindow(QWidget):
     build a new MathObject (a new mathematical object or property).
     """
 
+    send_pattern = Signal(MarkedPatternMathObject)
+
     def __init__(self, calc_patterns: [CalculatorPatternLines]):
         super().__init__()
         self.buttons_groups = []
@@ -101,14 +114,24 @@ class CalculatorMainWindow(QWidget):
 
         self.setLayout(main_lyt)
 
-    def buttons(self):
-        return sum([], [buttons_group.buttons
-                        for buttons_group in self.buttons_groups])
+        for btn in self.buttons():
+            btn.send_pattern.connect(self.process_clic)
+
+    def buttons(self) -> [CalculatorButton]:
+        btns = []
+        for buttons_group in self.buttons_groups:
+            btns.extend(buttons_group.buttons)
+        return btns
 
     def set_target(self, target):
         target = PatternMathObject(node='MVAR', info={}, children=[],
                                    math_type=target)
         self.target_label.set_target(target)
+
+    @Slot()
+    def process_clic(self, pattern):
+        self.send_pattern.emit(pattern)
+        # print('clac')
 
 
 class CalculatorController:
@@ -132,9 +155,20 @@ class CalculatorController:
 
         self.calculator_ui = CalculatorMainWindow(self.calculator_groups)
         self.calculator_ui.set_target(target)
+        self.calculator_ui.send_pattern.connect(self.insert_pattern)
 
     def show(self):
         self.calculator_ui.show()
+
+    @Slot()
+    def insert_pattern(self, pattern_s):
+        # Fixme: pattern or patterns?
+        # print('cloc')
+        ok = self.target.insert(pattern_s)
+        if ok:
+            self.calculator_ui.set_target(self.target)
+
+        print(ok)
 
 
 def main():
