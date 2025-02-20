f7ffe39db88005ee83cce3a7d9e9b1bc1dc159e0
Frederic Le Roux <frederic.le-roux@imj-prg.fr>
2025-02-20 18:23:13 +0100

    code:dev> Show statement at beginning

--- src/deaduction/dui/stages/exercise/Coordinator.py
+++ src/deaduction/dui/stages/exercise/Coordinator.py
@@ -266,8 +266,9 @@ class Coordinator(QObject):
         proof_state = self.exercise.initial_proof_state
         if proof_state:
             goal = proof_state.goals[0]
-            # goal.name_bound_vars()
             self.emw.ecw.update_goal(goal, [], history_nb=0)
+            if not exercise.negate_statement:
+                self.show_exercise_statement()
         elif exercise.negate_statement:
             ##############################################
             # We need initial proof state to negate goal #
@@ -785,10 +786,12 @@ class Coordinator(QObject):
         # esw = ExerciseStatementWindow(self.proof_step.goal,
         #                               parent=self.emw)
         # esw.exec_()
+        if not self.exercise.initial_proof_state:
+            return
         if not self.statement_window:
-            self.statement_window = ExerciseStatementWindow(
-                                                        self.proof_step.goal,
-                                                        parent=self.emw)
+            goal = self.exercise.initial_proof_state.goals[0]
+            self.statement_window = ExerciseStatementWindow(goal,
+                                                            parent=self.emw)
             geometry = self.emw.frameGeometry()
             scale_geometry(geometry, h_factor=.9, v_factor=0.6)
             self.statement_window.setGeometry(geometry)
